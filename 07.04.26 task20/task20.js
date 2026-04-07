d5318b3124ea263b5ff21710aa5c10c7617bd44d
Sadham1412 <127112415+Sadham1412@users.noreply.github.com>
2026-04-07 18:01:50 +0530

    Add files via upload

---
+++ 07.04.26 task20/task20.js
@@ -0,0 +1,18 @@
+let array=[1,2,3,4,5];
+array.push(6);
+console.log("push adding last element in the list");
+console.log(array);
+array.pop();
+console.log("pop function remove last element");
+console.log(array);
+array.unshift(10);
+console.log("unshift means adding first in a list ");
+console.log(array);
+console.log("shitf remove first value ");
+array.shift()
+console.log(array)
+console.log("Print all elements of an array using for loop.");
+let array1=[1,2,3,4,5,6,7,8,9,10]
+for(let i=0;i<=array1.length;i++){
+}
+ console.log(array1);
